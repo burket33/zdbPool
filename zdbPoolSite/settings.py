fb12e15869e7890b6fd40809cec7e852611f3923
Tim Burke <burke.timj@gmail.com>
2018-09-17 13:09:41 -0400

    Updated Settings for email storage

--- zdbPoolSite/settings.py
+++ zdbPoolSite/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "static/sent_emails")
